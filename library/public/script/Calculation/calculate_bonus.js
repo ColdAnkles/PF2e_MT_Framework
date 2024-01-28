8bc310c10f25892c0d9d4eed9275e753a5f9abd0
ColdAnkles <13864745+ColdAnkles@users.noreply.github.com>
2024-01-28 14:20:15 +1030

    Effect Bonuses
    Added additional argument for consuming bonuses
    Displaying bonuses does not consume the bonus
    Active effects that are used will be consumed if told to and effect is consumable

--- library/public/script/Calculation/calculate_bonus.js
+++ library/public/script/Calculation/calculate_bonus.js
@@ -1,6 +1,6 @@
 "use strict";
 
-function calculate_bonus(token, bonusScopes){
+function calculate_bonus(token, bonusScopes, consume=true){
 	if (typeof(token)=="string"){
 		token = MapTool.tokens.getTokenByID(token);
 	}
@@ -13,36 +13,60 @@ function calculate_bonus(token, bonusScopes){
 
 	let activeConditions = JSON.parse(token.getProperty("conditionDetails"));
 	
+	let selectedModifiers = {"bonuses":{"circumstance":null,"status":null,"item":null,"none":null},"maluses":{"circumstance":null,"status":null,"item":null,"none":null}};
 	let returnData = {"bonuses":{"circumstance":0,"status":0,"item":0,"none":0},"maluses":{"circumstance":0,"status":0,"item":0,"none":0}};
 
 	for (var e in activeEffects){
-		let effectData = activeEffects[e];
+		let effectData = JSON.parse(JSON.stringify(activeEffects[e]));
 		let effectBonuses = get_effect_bonus(effectData, bonusScopes);
-		//MapTool.chat.broadcast(JSON.stringify(effectBonuses));
+		effectData.bonus = effectBonuses;
 		for (var type in effectBonuses){
 			let typeDict = effectBonuses[type];
 			for (var k in typeDict){
 				if (type=="bonuses" && typeDict[k]>returnData[type][k]){
 					returnData[type][k] = typeDict[k];
+					selectedModifiers[type][k]=effectData;
 				}else if (type=="maluses" && typeDict[k]<returnData[type][k]){
 					returnData[type][k] = typeDict[k];
+					selectedModifiers[type][k]=effectData;
 				}
 			}
 		}
 	}
 
 	for (var c in activeConditions){
-		let conditionData = activeConditions[c];
+		let conditionData = JSON.parse(JSON.stringify(activeConditions[c]));
 		//MapTool.chat.broadcast(JSON.stringify(conditionData));
 		let conditionBonuses = get_effect_bonus(conditionData, bonusScopes);
+		conditionData.bonus = effectBonuses;
 		//MapTool.chat.broadcast(JSON.stringify(conditionBonuses));
 		for (var type in conditionBonuses){
 			let typeDict = conditionBonuses[type];
 			for (var k in typeDict){
 				if (type=="bonuses" && typeDict[k]>returnData[type][k]){
 					returnData[type][k] = typeDict[k];
+					selectedModifiers[type][k]=effectData;
 				}else if (type=="maluses" && typeDict[k]<returnData[type][k]){
 					returnData[type][k] = typeDict[k];
+					selectedModifiers[type][k]=effectData;
+				}
+			}
+		}
+	}
+
+	if(consume){
+		for(var t in selectedModifiers){
+			let modDict = selectedModifiers[t];
+			for(var m in modDict){
+				let modData = modDict[m];
+				if(modData!=null && modData.type=="effect"){
+					//MapTool.chat.broadcast(JSON.stringify(modData));
+					for(var r in modData.rules){
+						let removeAfter = modData.rules[r].removeAfterRoll;
+						if(removeAfter == "if-enabled" || removeAfter == "yes"){
+							toggle_named_effect(modData.name, token, 0);
+						}
+					}
 				}
 			}
 		}
--- library/public/script/Display/display_save_bonuses.js
+++ library/public/script/Display/display_save_bonuses.js
@@ -7,13 +7,13 @@ function display_save_bonuses(tokenID){
 	let reflex = Number(token.getProperty("reflex"));
 	//MapTool.chat.broadcast(String(token));
 	
-	let fortitudeBonus = calculate_bonus(tokenID,["fortitude","saving-throw"]);
+	let fortitudeBonus = calculate_bonus(tokenID,["fortitude","saving-throw"], false);
 	fortitudeBonus = fortitudeBonus.bonuses.circumstance + fortitudeBonus.bonuses.status + fortitudeBonus.bonuses.item + fortitudeBonus.maluses.circumstance + fortitudeBonus.maluses.status + fortitudeBonus.maluses.item;
 	
-	let reflexBonus = calculate_bonus(tokenID,["reflex","saving-throw"]);
+	let reflexBonus = calculate_bonus(tokenID,["reflex","saving-throw"], false);
 	reflexBonus = reflexBonus.bonuses.circumstance + reflexBonus.bonuses.status + reflexBonus.bonuses.item + reflexBonus.maluses.circumstance + reflexBonus.maluses.status + reflexBonus.maluses.item;
 	
-	let willBonus = calculate_bonus(tokenID,["will","saving-throw"]);
+	let willBonus = calculate_bonus(tokenID,["will","saving-throw"], false);
 	willBonus = willBonus.bonuses.circumstance + willBonus.bonuses.status + willBonus.bonuses.item + willBonus.maluses.circumstance + willBonus.maluses.status + willBonus.maluses.item;
 	
 	let returnString = "Fort: " + pos_neg_sign(fortitude + fortitudeBonus) + ", Ref: " + pos_neg_sign(reflex + reflexBonus) + ", Will: " + pos_neg_sign(will + willBonus);
