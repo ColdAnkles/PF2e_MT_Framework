5786078e958fd0ad87d6dda52dcb68cb9c77fad0
ColdAnkles <13864745+ColdAnkles@users.noreply.github.com>
2024-02-18 15:28:07 +1030

    Remove Macro Change

--- library/public/script/Helper/createMacro.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-function createMacro(props, tokenID) {
-    MTScript.setVariable("props", props);
-    MTScript.setVariable("tokenID", tokenID);
-    MTScript.evalMacro("[h: createMacro(props,tokenID)]");
-}
---
+++ library/public/script/Helper/metaMacros.js
@@ -0,0 +1,39 @@
+"use strict";
+
+function createMacro(props, tokenID) {
+    MTScript.setVariable("props", props);
+    MTScript.setVariable("tokenID", tokenID);
+    MTScript.evalMacro("[h: createMacro(props,tokenID)]");
+}
+
+function removeMacro(macroName, tokenID){
+    if (typeof (tokenID) != "string") {
+        tokenID = tokenID.getId();
+    }
+    
+    MTScript.setVariable("tokenID", tokenID);
+    MTScript.evalMacro("[h: allMacros = getMacros(\"json\", tokenID)]")
+    let allMacros = JSON.parse(MTScript.getVariable("allMacros"));
+    let removeIndicies = [];
+
+    for (var a in allMacros){
+        MTScript.setVariable("label", allMacros[a]);
+        MTScript.evalMacro("[h: indicies = getMacroIndexes(label, \"json\", tokenID)]");
+        let indexList = JSON.parse(MTScript.getVariable("indicies"));
+        let macroLabel = allMacros[a].replaceAll("&#9670;","").replaceAll(/<img.*<\/img>/g,"").replace(/^ /,"").replace(/ $/,"");
+        if(macroLabel==macroName){
+            for(var i in indexList){
+                if(!(removeIndicies.includes(indexList[i]))){
+                    removeIndicies.push(indexList[i]);
+                }
+            }
+        }
+    }
+
+    MapTool.chat.broadcast(JSON.stringify(removeIndicies));
+
+    for (var ri in removeIndicies){
+        MTScript.setVariable("index", removeIndicies[ri]);
+        MTScript.evalMacro("[h: removeMacro(index, tokenID)]");
+    }
+}
