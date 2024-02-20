c919e8a647640df3e071f3dac201cb0f9fbeb680
ColdAnkles <13864745+ColdAnkles@users.noreply.github.com>
2024-02-20 17:34:25 +1030

    AC Proficiency Calculation Fix

--- library/public/script/Calculation/calculate_ac.js
+++ library/public/script/Calculation/calculate_ac.js
@@ -19,23 +19,33 @@ function calculate_ac(tokenID) {
 		dexCap = bonuses.otherEffects.DexterityModifierCap;
 	}
 
-	let profBonus = 0
+	let profBonus = 0;
+	let armorProfType = "unarmored";
 
 	if(eqArmor!=null){
 		armorBonus = eqArmor.acBonus + eqArmor.runes.potency;
 		dexCap = Math.min(eqArmor.dexCap, dexCap);
-	}else{
-		for(var p in profs){
-			if("UNARMORED" == profs[p].name.toUpperCase()){
-				profBonus = profs[p].bonus;
-				break;
+		armorProfType = eqArmor.armorType;
+		if("otherEffects" in bonuses && eqArmor.baseItem in bonuses.otherEffects){
+			let armorChange = bonuses.otherEffects[eqArmor.baseItem];
+			if (armorChange.mode=="add"){
+				armorBonus+=armorChange.value;
 			}
 		}
 	}
+	
+	for(var p in profs){
+		if(armorProfType == profs[p].name.toLowerCase()){
+			profBonus = profs[p].bonus;
+			break;
+		}
+	}
+	
 
 	dex_bonus = Math.max(0, Math.min(dex_bonus, dexCap));
 
 	bonuses = bonuses.bonuses.circumstance + bonuses.bonuses.status + Math.max(bonuses.bonuses.item, armorBonus) + bonuses.maluses.circumstance + bonuses.maluses.status + bonuses.maluses.item + bonuses.bonuses.proficiency;
+	//MapTool.chat.broadcast(String(base_ac) + "+"+String(bonuses) + "+" + String(dex_bonus) + "+"+String(profBonus));
 	let totalAC = (base_ac + bonuses + dex_bonus + profBonus);
 	return totalAC;
 }
