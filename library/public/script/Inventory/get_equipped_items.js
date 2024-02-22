915813a73effb072fb51b59d967fdeb12b65b117
ColdAnkles <13864745+ColdAnkles@users.noreply.github.com>
2024-02-22 20:19:42 +1030

    Add Find Handwraps Function for Unarmed Attacks

--- library/public/script/Inventory/get_equipped_items.js
+++ library/public/script/Inventory/get_equipped_items.js
@@ -12,6 +12,7 @@ function get_equipped_shield(token) {
 		}
 		//MapTool.chat.broadcast(JSON.stringify(itemData));
 	}
+	return null;
 }
 
 MTScript.registerMacro("ca.pf2e.get_equipped_shield", get_equipped_shield);
@@ -28,6 +29,25 @@ function get_equipped_armor(token) {
 		}
 		//MapTool.chat.broadcast(JSON.stringify(itemData));
 	}
+	return null;
 }
 
 MTScript.registerMacro("ca.pf2e.get_equipped_armor", get_equipped_armor);
+
+function find_handwraps(token){
+	let bestHandwraps = null;
+	let bestHandwrapsLevel = -1;
+	if (typeof (token) == "string") {
+		token = MapTool.tokens.getTokenByID(token);
+	}
+	let inventory = JSON.parse(token.getProperty("inventory"));
+	for (var i in inventory) {
+		let itemData = inventory[i];
+		if (itemData.name == "Handwraps of Mighty Blows" && itemData.level > bestHandwrapsLevel) {
+			bestHandwraps = itemData;
+		}
+	}
+	return bestHandwraps;
+}
+
+MTScript.registerMacro("ca.pf2e.find_handwraps", find_handwraps);
