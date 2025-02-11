1401c932294862bc040df6739c240cab66483f3e
ColdAnkles <13864745+ColdAnkles@users.noreply.github.com>
2025-02-11 20:19:13 +1030

    Speed Bonus Display Fix

--- library/public/script/Display/display_speeds.js
+++ library/public/script/Display/display_speeds.js
@@ -19,16 +19,53 @@ function display_speeds(token) {
 		}
 	}
 
-	let speedBonus = calculate_bonus(token, ["speed", "land-speed", "all-speeds"]);
-	speedBonus = speedBonus.bonuses.circumstance + speedBonus.bonuses.status + speedBonus.bonuses.item + speedBonus.bonuses.none + speedBonus.maluses.circumstance + speedBonus.maluses.status + speedBonus.maluses.item + speedBonus.maluses.none;
+	let speedBonusRaw = null;
+	let speedBonus = 0
+	try {
+		speedBonusRaw = calculate_bonus(token, ["speed", "land-speed", "all-speeds"]);
+		speedBonus = speedBonus + speedBonusRaw.bonuses.circumstance + speedBonusRaw.bonuses.status + speedBonusRaw.bonuses.item + speedBonusRaw.bonuses.none + speedBonusRaw.maluses.circumstance + speedBonusRaw.maluses.status + speedBonusRaw.maluses.item + speedBonusRaw.maluses.none;
+
+		if ("otherEffects" in speedBonusRaw && "land-speed" in speedBonusRaw.otherEffects) {
+			if ("value" in speedBonusRaw.otherEffects["land-speed"]) {
+				speedBonus += speedBonusRaw.otherEffects["land-speed"].value;
+			} else {
+				speedBonus += speedBonusRaw.otherEffects["land-speed"];
+			}
+		}
+	} catch (e) {
+		MapTool.chat.broadcast("Error in display_speeds during land-speed-calc");
+		MapTool.chat.broadcast("token: " + String(token));
+		MapTool.chat.broadcast("speedBonus: " + String(speedBonus));
+		MapTool.chat.broadcast("speedBonusRaw: " + JSON.stringify(speedBonusRaw));
+		MapTool.chat.broadcast("" + e + "\n" + e.stack);
+		return;
+	}
 
 	let otherSpeedString = [];
 
-	for (var s in otherSpeed) {
-		//MapTool.chat.broadcast(JSON.stringify(otherSpeed[s]));
-		speedBonus = calculate_bonus(token, ["speed", otherSpeed[s].type, "all-speeds"]);
-		speedBonus = speedBonus.bonuses.circumstance + speedBonus.bonuses.status + speedBonus.bonuses.item + speedBonus.bonuses.none + speedBonus.maluses.circumstance + speedBonus.maluses.status + speedBonus.maluses.item + speedBonus.maluses.none;
-		otherSpeedString.push(otherSpeed[s].type + " " + String(Math.max(Number(otherSpeed[s].value) + speedBonus + speedPenalty, 5)) + " ft");
+	try {
+		for (var s in otherSpeed) {
+			//MapTool.chat.broadcast(JSON.stringify(otherSpeed[s]));
+			speedBonusRaw = calculate_bonus(token, ["speed", otherSpeed[s].type, "all-speeds"]);
+			let otherSpeedBonus = speedBonusRaw.bonuses.circumstance + speedBonusRaw.bonuses.status + speedBonusRaw.bonuses.item + speedBonusRaw.bonuses.none + speedBonusRaw.maluses.circumstance + speedBonusRaw.maluses.status + speedBonusRaw.maluses.item + speedBonusRaw.maluses.none;
+
+			if ("otherEffects" in speedBonusRaw && otherSpeed[s].type in speedBonusRaw.otherEffects) {
+				if ("value" in speedBonusRaw.otherEffects[otherSpeed[s].type]) {
+					otherSpeedBonus += speedBonusRaw.otherEffects[otherSpeed[s].type].value;
+				} else {
+					otherSpeedBonus += speedBonusRaw.otherEffects[otherSpeed[s].type];
+				}
+			}
+
+			otherSpeedString.push(otherSpeed[s].type + " " + String(Math.max(Number(otherSpeed[s].value) + otherSpeedBonus + speedPenalty, 5)) + " ft");
+		}
+	} catch (e) {
+		MapTool.chat.broadcast("Error in display_speeds during other-speed-calc");
+		MapTool.chat.broadcast("token: " + String(token));
+		MapTool.chat.broadcast("speedBonus: " + String(speedBonus));
+		MapTool.chat.broadcast("speedBonusRaw: " + JSON.stringify(speedBonusRaw));
+		MapTool.chat.broadcast("" + e + "\n" + e.stack);
+		return;
 	}
 
 	otherSpeedString = otherSpeedString.join(", ");
