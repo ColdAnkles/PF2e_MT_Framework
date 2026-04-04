b0e07dff84867ba6818ec248556ef0bc99ea45ba
ColdAnkles <13864745+ColdAnkles@users.noreply.github.com>
2026-04-04 14:53:46 +1030

    Dazzle Check Fix

--- library/public/script/Action/dazzle_check.js
+++ library/public/script/Action/dazzle_check.js
@@ -1,11 +1,14 @@
 "use strict";
 
 function dazzle_check(token) {
+    //Returns True if Succeeds Dazzle Check or is not Dazzled
     let activeConditions = JSON.parse(token.getProperty("conditionDetails"));
 
     try {
         if ("Dazzled" in activeConditions) {
             return flat_check(token, { "dc": 5, "altTitle": token.getName().replace("Lib:","") + " is dazzled.", "failMsg": token.getName().replace("Lib:","") + " is too dazzled and fails to act." });
+        }else{
+            return true;
         }
     } catch (e) {
         MapTool.chat.broadcast("Error in dazzle_check");
