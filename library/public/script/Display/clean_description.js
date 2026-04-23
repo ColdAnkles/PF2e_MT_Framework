8b6e17250f8d3a85f615e20c208e19749573795a
ColdAnkles <13864745+ColdAnkles@users.noreply.github.com>
2026-04-23 16:29:50 +0930

    Act Parse Fix

--- library/public/script/Display/clean_description.js
+++ library/public/script/Display/clean_description.js
@@ -558,7 +558,7 @@ function clean_description(description, removeLineBreaks = true, removeHR = true
 
 	cleanDescription = cleanDescription.replaceAll(/\[\[\/act.*\]\]{(.*)}/g, "$1");
 
-	cleanDescription = cleanDescription.replaceAll(/\[\[\/act(.*)\]\]/g, "$1");
+	cleanDescription = cleanDescription.replaceAll(/\[\[\/act ([^=]*)[\s\]]+.*\]/g, "$1");
 
 	cleanDescription = cleanDescription.replaceAll(/\[(.*)\|.*\]/g, " $1 ");
 
